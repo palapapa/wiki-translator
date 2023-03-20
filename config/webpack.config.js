f34802c02f7da255455bc8adff49d48de848528d
palapapa <palapapa93@gmail.com>
2023-03-20 12:50:09 +0800

    Made webpack.config.js use ESM

---
+++ config/webpack.config.js
@@ -0,0 +1,34 @@
+import path from "path";
+import glob from "glob";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default
+{
+    entry: Object.fromEntries
+    (
+        glob.sync
+        (
+            path.resolve
+            (
+                __dirname, "../src/**/*.ts"
+            ).replaceAll("\\", "/") // path.resolve returns Windows path. It is converted to POSIX path here.
+        ).map
+        (
+            (p) => p.replaceAll("\\", "/") // Same for glob.sync
+        ).map
+        (
+            (p) =>
+            {
+                let filename = p.split("src/")[1];
+                return [filename.substring(0, filename.lastIndexOf(".")) + ".js", p];
+            } 
+        )
+    ),
+    output:
+    {
+        filename: "[name]"
+    }
+};
