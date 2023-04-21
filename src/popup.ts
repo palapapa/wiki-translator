5b495736f95ce0c719f6a27bdf170a26c5987026
palapapa <palapapa93@gmail.com>
2023-04-22 04:09:18 +0800

    Removed uses of the null assertion operator and corrected typo
    Removed all uses of the null assertion operator in popup.ts and
    corrected a typo in popup.html from "origianl" to "original" and another
    in popup.ts from "btnOutPut" to "btnOutput".

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,8 @@
 function Dropmenu_show(): void {
-    document.getElementById("Dropdown")!.classList.toggle("show");
+    const dropdown = document.getElementById("Dropdown");
+    if (dropdown !== null) {
+        dropdown.classList.toggle("show")
+    }
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -16,41 +19,60 @@ window.onclick = function (event: MouseEvent): void {
 }
 
 function Dropmenu_update(language: string): void {
-    document.getElementById('target-language-text')!.innerHTML = ("Target Language: " + language);
-    if (language === "Auto detect") {
-        document.getElementById('target-language-btn_text')!.innerHTML = "Detected Language";
+    const targetLanguageText = document.getElementById('target-language-text');
+    if (targetLanguageText !== null) {
+        targetLanguageText.innerHTML = ("Target Language: " + language);
     }
-    else {
-        document.getElementById('target-language-btn_text')!.innerHTML = language;
+    const targetLanguageBtnText = document.getElementById('target-language-btn_text');
+    if (targetLanguageBtnText !== null) {
+        if (language === "Auto detect") {
+            targetLanguageBtnText.innerHTML = "Detected Language";
+        }
+        else {
+            targetLanguageBtnText.innerHTML = language;
+        }
     }
+    
 }
 
 // 開啟 target language 下拉選單
-const btnOutPut = document.getElementById("dropbtnn")!;
-btnOutPut.onclick = Dropmenu_show;
+const btnOutput = document.getElementById("dropbtnn");
+if (btnOutput !== null) {
+    btnOutput.onclick = Dropmenu_show;
+}
 
 // 各語言選項
-const target_auto = document.getElementById("auto")!;
-target_auto.onclick = function () {
-    Dropmenu_update("Auto detect");
-};
+const target_auto = document.getElementById("auto");
+if (target_auto !== null) {
+    target_auto.onclick = function () {
+        Dropmenu_update("Auto detect");
+    };
+}
 
-const target_english = document.getElementById("english")!;
-target_english.onclick = function () {
-    Dropmenu_update("English");
-};
+const target_english = document.getElementById("english");
+if (target_english !== null) {
+    target_english.onclick = function () {
+        Dropmenu_update("English");
+    };
+}
 
-const target_chinese = document.getElementById("chinese")!;
-target_chinese.onclick = function () {
-    Dropmenu_update("中文");
-};
+const target_chinese = document.getElementById("chinese");
+if (target_chinese !== null) {
+    target_chinese.onclick = function () {
+        Dropmenu_update("中文");
+    };
+}
 
-const target_spanish = document.getElementById("spanish")!;
-target_spanish.onclick = function () {
-    Dropmenu_update("Español");
-};
+const target_spanish = document.getElementById("spanish");
+if (target_spanish !== null) {
+    target_spanish.onclick = function () {
+        Dropmenu_update("Español");
+    };
+}
 
-const target_malayalam = document.getElementById("malayalam")!;
-target_malayalam.onclick = function () {
-    Dropmenu_update("മലയാളം");
-};
+const target_malayalam = document.getElementById("malayalam");
+if (target_malayalam !== null) {
+    target_malayalam.onclick = function () {
+        Dropmenu_update("മലയാളം");
+    };
+}
