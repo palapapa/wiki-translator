344ba555656ed115b76e6efbf569be6671e1e954
palapapa <palapapa93@gmail.com>
2023-06-12 21:30:41 +0800

    Made translationReplacer remove the div with id siteNotice
    When this div is present on the page, it causes the translated article
    to appear before the original one without replacing it, and then it will
    completely cover the translated portion.

--- src/translationReplacer.ts
+++ src/translationReplacer.ts
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(
     (message: string) => {
-        console.log("here");
+        document.getElementById("siteNotice")?.remove(); // This element causes the translated article to appear before the original one without replacing it and then it will completely cover the translated portion
         const content = document.getElementsByClassName("mw-parser-output")[0];
         if (content !== undefined) {
             content.innerHTML = message;
